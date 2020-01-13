0705c96ec58b924eda88feb58d2423bda5685885
kazup1125 <kazup1125@me.com>
2020-01-13 14:22:01 +0900

    Initial Commit

---
+++ kazuyaProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kazuyaProject
+//
+//  Created by 国分和弥 on 2020/01/13.
+//  Copyright © 2020 Kazuya Kokubun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
